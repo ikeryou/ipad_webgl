7af447c654e6ecb252f2ae351f30c22179cd35a8
ikeryou <ikeda.ryou@gmail.com>
2020-06-20 23:50:05 +0900

    test

--- docs/script.js
+++ docs/script.js
@@ -2,8 +2,6 @@
 let _renderer, _mainScene, _mainCamera, _con, _light
 let _mesh = []
 let _cnt = 0
-let _tgPos = new THREE.Vector2()
-let _tgScale = 1
 let _mouse = new THREE.Vector2(0, 0)
 
 
@@ -11,13 +9,14 @@ let _mouse = new THREE.Vector2(0, 0)
 // 初期化
 // -----------------
 function init() {
+	
   // make renderer
   _renderer = new THREE.WebGLRenderer({
     antialias: false,
     powerPreference: 'low-power',
     canvas: document.getElementsByClassName('kv')[0]
   })
-  _renderer.setClearColor(0xff0000, 1) // 背景色
+  _renderer.setClearColor(0x000000, 1) // 背景色
   _renderer.setPixelRatio(window.devicePixelRatio || 1)
 
   // make main scene
@@ -35,28 +34,40 @@ function init() {
   _con = new THREE.Object3D()
   _mainScene.add(_con)
 
-  const seg = 1
-  for(let i = 0; i < 15; i++) {
+  for(let i = 0; i < 10; i++) {
+	  
+	let color = new THREE.Color(random(0, 1), random(0, 1), random(0, 1))
+	let color2 = new THREE.Color(random(0, 1), random(0, 1), random(0, 1))
+	  
+	const seg = 32
     const mesh = new THREE.Mesh(
-      new THREE.BoxBufferGeometry(1,1,1,seg,seg,seg),
+      new THREE.SphereBufferGeometry(0.5,seg),
+	//   new THREE.MeshBasicMaterial({
+	// 	color:color,
+    //     side: THREE.DoubleSide,
+	// 	transparent:true,
+	// 	opacity:1,
+    //     wireframe:true
+	// })
       new THREE.MeshPhongMaterial({
-        color:0x000000,
-        specular:0x000000,
+        color:color,
+        specular:color2,
         side: THREE.DoubleSide,
-        wireframe:(Math.random() > 0.5)
+		// transparent:true,
+		depthTest:false,
+        wireframe:false
       })
     )
     _con.add(mesh)
+	
     _mesh.push({
       mesh:mesh,
-      noise:random(0.8, 2.1),
-      size:new THREE.Vector3(1,1,1),
-      rot:new THREE.Vector3(1,1,1),
-      scale:1,
-      superNoise:Math.random() < 0.1
+      noise:random(1, 2)
     })
+	
   }
-
+	
+  // マウス位置
   window.addEventListener('mousemove', e => {
     const w = window.innerWidth
     const h = window.innerHeight
@@ -80,109 +91,42 @@ function init() {
 // 更新
 // -----------------
 function update() {
+	
   const w = window.innerWidth
   const h = window.innerHeight
-
-  // 一定間隔でアクション
-  let changeFlg = false
-  if(_cnt % 60 == 0) {
-    _tgScale = 1.25
-    _con.scale.set(_tgScale, _tgScale, _tgScale)
-    changeFlg = true
-    _light.position.x = random(-w, w)
-    _light.position.y = random(-w, w)
-    _light.position.z = random(-w, w)
-    resize()
-  }
-
-
-
-  const ease = 0.1
-
+  
+  // 中央までの距離
+  const max = 1 / Math.cos(radian(45))
+  let d = Math.sqrt(_mouse.x * _mouse.x + _mouse.y * _mouse.y)
+  d = map(d, 0, 1, 0, max * 0.6)
+  
   const len = _mesh.length
   for(let i = 0; i < len; i++) {
 
     const mesh = _mesh[i].mesh
-    const size = _mesh[i].size
-    const rot = _mesh[i].rot
-    let noise = _mesh[i].noise
-
-
-
-    // 目標に向かってだんだん近くことで移動感のあるゆらゆらに
-    let x = _tgPos.x * noise + w * _mouse.x * -0.5
-    let y = _tgPos.y * noise + w * _mouse.y * 0.5
-
-    // if(_mesh[i].superNoise) {
-    //   x *= 2
-    //   y *= 2
-    // }
-
-    mesh.position.x += (x - mesh.position.x) * ease
-    mesh.position.y += (y - mesh.position.y) * ease
-    // mesh.position.z += (y - mesh.position.z) * ease
-
-    // 一定間隔で目標位置を変更
-    if(_cnt % 3 == 0) {
-      const range = w * 0.01;
-      _tgPos.x = random(-range, range)
-      _tgPos.y = random(-range, range)
-    }
-
-    // 一定間隔でアクション
-    if(changeFlg) {
-      mesh.material.wireframe = Math.random() < 0.5
-
-      hBase = random(0, 360)
-      c = new THREE.Color('hsl(' + ~~(hBase) + ', 100%, 50%)')
-      c2 = new THREE.Color(1 - c.r, 1 - c.g, 1 - c.b)
-      mesh.material.color = c
-      mesh.material.specular = c2
-      // mesh.material.color = new THREE.Color(Math.random(), Math.random(), Math.random())
-      // mesh.material.specular = new THREE.Color(Math.random(), Math.random(), Math.random())
-      // mesh.material.emissive = new THREE.Color(Math.random(), Math.random(), Math.random())
-
-      _mesh[i].scale = random(5, 10)
-    }
-
-    mesh.rotation.x = rot.x
-    mesh.rotation.y = rot.y
-    mesh.rotation.z = rot.z
-
-    // const ease2 = Math.pow(0.5, i)
-    // _mesh[i].scale += (1 - _mesh[i].scale) * ease
-    //   const s = 3
-    //
-    // mesh.scale.x += (size.x * s - mesh.scale.x) * ease
-    // mesh.scale.y += (size.y * s - mesh.scale.y) * ease
-    // mesh.scale.z += (size.z * s - mesh.scale.z) * ease
-
-    // この辺は雰囲気づくり
-    // 適当に値を流用
-    // mesh.rotation.x += ((_tgScale - 1) - mesh.rotation.x) * ease
-    // mesh.rotation.y += ((_tgScale - 1) - mesh.rotation.y) * ease
-    // mesh.rotation.z += ((_tgScale - 1) - mesh.rotation.z) * ease
+	const key = map(i, 0, 1, 0, len - 1)
+	let op = Math.abs(key - d)
+    // mesh.material.opacity = op
+	
+	// mesh.position.x = (_mouse.x - key) w * 0.1 * d
+	
+	const size = w * mix(0.001, 0.25, Math.pow(op, i + 1))
+	mesh.scale.set(size, size, size)
   }
-
-  // 常に1にだんだん近く
-  _tgScale += (1 - _tgScale) * ease
-
-  // 目標スケールにだんだん近く
-  const tgSize = _tgScale
-  _con.scale.x += (tgSize - _con.scale.x) * ease
-  _con.scale.y += (tgSize - _con.scale.y) * ease
-  _con.scale.z += (tgSize - _con.scale.z) * ease
-
-  let tgX = _mouse.x * w * 0.25
-  let tgY = _mouse.y * h * 0.25
-  // tgX *= mix(, 1, _mouse.distanceTo(new THREE.Vector3(0,0,0)))
+	
+  const ease = 0.1 
+  let tgX = _mouse.x * w * 0.5
+  let tgY = _mouse.y * h * 0.5
   _con.position.x += (tgX - _con.position.x) * ease
   _con.position.y += (tgY * -1 - _con.position.y) * ease
+  
+  _light.position.x = _con.position.x * -1
+  _light.position.y = _con.position.y * -1
+  _light.position.z = 1000
 
   _con.rotation.x += 0.001
-  // _con.rotation.y += 0.001
   _con.rotation.y = _con.position.y * 0.1 * (Math.PI / 180)
-  _con.rotation.z = _con.position.x * 0.1 * (Math.PI / 180)
+  _con.rotation.z = _con.position.x * -0.1 * (Math.PI / 180)
 
   _cnt++
 
@@ -205,29 +149,6 @@ function resize() {
   _mainCamera.position.z = (height * 0.5) / Math.tan((_mainCamera.fov * 0.5) * Math.PI / 180)
 
   _renderer.setSize(width, height)
-
-
-  for(let i = 0; i < _mesh.length; i++) {
-    let size = width * 0.05 * _mesh[i].noise
-    if(Math.random() < 0.2) {
-      size *= 10
-    }
-    if(Math.random() < 0.5) {
-      size *= 0.2
-    }
-    _mesh[i].mesh.scale.set(size, Math.max(width, height) * 3.5, size)
-    // _mesh[i].size.set(size, Math.max(width, height) * 1.5, size)
-    // _mesh[i].mesh.rotation.z = random(-90, 90) * (Math.PI / 180)
-    // _mesh[i].mesh.rotation.x = random(-90, 90) * (Math.PI / 180)
-    // _mesh[i].mesh.rotation.y = random(-90, 90) * (Math.PI / 180)
-
-    _mesh[i].rot.set(
-      random(-90, 90) * (Math.PI / 180),
-      random(-90, 90) * (Math.PI / 180),
-      random(-90, 90) * (Math.PI / 180)
-    )
-
-  }
 }
 
 
@@ -243,5 +164,41 @@ function mix(x, y, a) {
   return x * (1 - a) + y * a
 }
 
+
+// ----------------------------------------
+// 範囲変換
+// @val     : 変換したい値
+// @toMin   : 変換後の最小値
+// @toMax   : 変換後の最大値
+// @fromMin : 変換前の最小値
+// @fromMax : 変換前の最大値
+// ----------------------------------------
+function map(val, toMin, toMax, fromMin, fromMax) {
+  if(val <= fromMin) {
+    return toMin;
+  }
+  if(val >= fromMax) {
+    return toMax;
+  }
+  p = (toMax - toMin) / (fromMax - fromMin);
+  return ((val - fromMin) * p) + toMin;
+}
+
+
+// ----------------------------------------
+// 度からラジアンに変換
+// @val : 度
+// ----------------------------------------
+function radian(val) {
+  return val * Math.PI / 180;
+}
+
+// ----------------------------------------
+// ラジアンから度に変換
+// @val : ラジアン
+// ----------------------------------------
+function degree(val) {
+  return val * 180 / Math.PI;
+}
 
 init()
